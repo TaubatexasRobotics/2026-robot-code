191e7438f3a43a3e5869f2ae5abcecaf36e8ea55
Daniel <danirmendo@gmail.com>
2026-01-15 20:35:33 -0300

    add the pid controller

--- intake.py
+++ intake.py
@@ -1,23 +1,31 @@
 import wp
 import wpilib.drive
 import rev
+import wpimath.controller
+
 
 
 class Intake:
 
     def __init__(self):
+
+        self.arm_motor = rev.SparkMax(1, rev.SparkLowLevel.MotorType.kBrushless)
+        self.roller_motor = rev.SparkMax(2, rev.SparkLowLevel.MotorType.kBrushless)
+
+        self.arm_pid = wpimath.controller.PIDController(0.1, 0.0, 0.0)
+
         
-        self.intake_motor = rev.SparkMax(1, rev.SparkLowLevel.MotorType.kBrushless)
-        self.intake_retract_motor = rev.SparkMax(2, rev.SparkLowLevel.MotorType.kBrushless)
-
-     
-     
-
     def turnOnIntake(self):
-       
-        self.intake_motor.set(1)
-        self.intake_retract_motor.set(-0.5)
+        setpoint = 15
+    
+        self.arm_motor.set(self.arm_pid.calculate(self.arm_motor.getEncoder().getPosition(), setpoint))
+        self.roller_motor.set(1)
 
-    def raiseIntake(self):
-        self.intake_retract_motor.set(0.4)
-        self.intake_motor.set(0)
+    def turnOffIntake(self):
+        setpoint = 0
+        self.arm_motor.set(self.arm_pid.calculate(self.arm_motor.getEncoder().getPosition(), setpoint))
+        self.roller_motor.set(0)  
+
+
+
+       
