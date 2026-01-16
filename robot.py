4e31b80d9b2d64ec474e2cebb4a0bbf7fecbf66e
Danone <luccavanone1@gmail.com>
2026-01-15 21:46:48 -0300

    AprilTag sistem

--- robot.py
+++ robot.py
@@ -1,2 +1,14 @@
-import wpilib
+from wpilib import TimedRobot
+from drivetrain import Drivetrain
+from camera import AprilTagCamera
 
+class Robot(TimedRobot):
+    def robotInit(self) -> None:
+        self.drivetrain = Drivetrain()
+        
+
+    def teleopInit(self) -> None:
+        pass
+    
+    def teleopPeriodic(self) -> None:
+        self.drivetrain.arcadeDriveAlign(3)
