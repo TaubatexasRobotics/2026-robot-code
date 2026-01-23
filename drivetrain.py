7e579ed1a66266bbe955c7988ba20f2ebbe6d1c4
Guilherme Samuel <guisamuel800@gmail.com>
2026-01-23 11:52:49 -0300

    wpi: building odometry

--- drivetrain.py
+++ drivetrain.py
@@ -1,29 +1,61 @@
-import wpilib
-import wpimath.controller
-import rev
-import wpilib.drive
-import navx
 import constants
+
+from commands2 import Subsystem
 from typing import Optional
-import phoenix5
 from camera import AprilTagCamera
+from phoenix5 import WPI_VictorSPX
+from wpilib import MotorControllerGroup, DriverStation
+from navx import AHRS
+from wpilib.drive import DifferentialDrive
+from wpimath.controller import PIDController
+from wpimath.geometry import Pose2d
+from pathplannerlib.auto import AutoBuilder
+from pathplannerlib.controller import PPLTVController
+from pathplannerlib.config import RobotConfig
+from wpimath.kinematics import DifferentialDriveOdometry
 
-class Drivetrain:
+class Drivetrain(Subsystem):
     def __init__(self) -> None:
-        self.left_front_motor = phoenix5.WPI_VictorSPX(constants.kLeftFrontId)
-        self.left_back_motor = phoenix5.WPI_VictorSPX(constants.kLeftBackId)
-        self.right_front_motor = phoenix5.WPI_VictorSPX(constants.kRightFrontId)
-        self.right_back_motor = phoenix5.WPI_VictorSPX(constants.kRightBackId)
+        self.left_front_motor = WPI_VictorSPX(constants.kLeftFrontId)
+        self.left_back_motor = WPI_VictorSPX(constants.kLeftBackId)
+        self.right_front_motor = WPI_VictorSPX(constants.kRightFrontId)
+        self.right_back_motor = WPI_VictorSPX(constants.kRightBackId)
 
-        self.left_motors = wpilib.MotorControllerGroup(self.left_front_motor,self.left_back_motor)
-        self.right_motors = wpilib.MotorControllerGroup(self.right_front_motor,self.right_back_motor)
+        self.left_motors = MotorControllerGroup(self.left_front_motor,self.left_back_motor)
+        self.right_motors = MotorControllerGroup(self.right_front_motor,self.right_back_motor)
         self.right_motors.setInverted(True)
-        self.drivetrain = wpilib.drive.DifferentialDrive(self.left_motors,self.right_motors)
+        self.drivetrain = DifferentialDrive(self.left_motors,self.right_motors)
 
-        self.navx = navx.AHRS.create_spi()
-        self.pid_angular = wpimath.controller.PIDController(0.1, 0, 0)
-        self.pid_forward = wpimath.controller.PIDController(0.1, 0, 0)
-        self.camera = AprilTagCamera("Camera7459")
+        self.navx = AHRS.create_spi()
+        self.pid_angular = PIDController(0.1, 0, 0)
+        self.pid_forward = PIDController(0.1, 0, 0)
+        self.camera = AprilTagCamera(constants.kCameraName)
+
+        self.pose = Pose2d()
+        self.odometry = DifferentialDriveOdometry(
+            self.gyro.getRotation2d(),
+
+        )
+
+        AutoBuilder.configure(
+            self.getPose,
+            self.resetPose,
+            self.getRobotRelativeSpeeds,
+            lambda: speeds, feedforwards: self.driveRobotRelative(speeds)
+            PPLTVController(0.02),
+            config,
+            self.shouldFlipPath,
+            self
+        )
+
+    def shouldFlipPath():
+        return DriverStation.getAlliance() == DriverStation.Alliance.kRed
+
+    def getPose(self) -> Pose2d:
+        return self.pose
+    
+    def resetPose(self) -> None:
+        
 
     def Front(self) -> None:
         self.drivetrain.tankDrive(1,1)
@@ -44,7 +76,7 @@ class Drivetrain:
     
     def arcadeDriveAimAndRange(self, tag: int) -> None:
         yaw, range = self.camera.getYawWithRange(tag)
-        range = self.pid_forward.calculate(range, constants.kGoalRangeMeters)
+        range = self.pid_forward.calculate(range, constants.kGoalRangeMeters) if yaw != -1 else 0
         rotation = self.pid_angular.calculate(yaw, 0)
         self.drivetrain.arcadeDrive(range, rotation)
     
