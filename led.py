e71ae5977f9d60b6dc2169c1951016c9ea0e7eca
Guilherme Samuel <guisamuel800@gmail.com>
2026-01-24 14:28:15 -0300

    feat: create specific colors function

--- led.py
+++ led.py
@@ -3,4 +3,21 @@ import constants
 
 class LEDController:
     def __init__(self) -> None:
-        self.arduino = SerialPort(constants.kBaudRate, constants.kLEDUSBPort)
+        self.arduino = SerialPort(
+            constants.kBaudRate, 
+            constants.kLEDUSBPort
+        )
+
+    def activateRedColor(self) -> None:
+        changeColor('r')
+
+    def activateGreenColor(self) -> None:
+        changeColor('g')
+
+    def activateBlueColor(self) -> None:
+        changeColor('b')
+
+    def changeColor(char: str) -> None:
+        byte_obj = char.encode('ascii')
+        self.arduino.write(byte_obj)
+
