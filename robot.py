040b64e3c4a899b458db59c12851e0744ccbe4a1
Daniel TI <danirmendo@gmail.com>
2026-01-30 21:09:16 -0300

    PId 2.0 (now it's working)

--- robot.py
+++ robot.py
@@ -1,2 +1,16 @@
 import wpilib
+from intake import Intake
 
+class MyRobot(wpilib.TimedRobot):
+
+    def robotInit(self):
+        self.intake = Intake()
+        self.joystick = wpilib.Joystick(0)
+
+    def teleopPeriodic(self):
+        if self.joystick.getRawButton(1):
+          self.intake.testeMotor()
+        elif self.joystick.getRawButton(2):
+          self.intake.Contrario()
+        else:
+            self.intake.arm_motor.set(0)
