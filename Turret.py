ab2c1b68633fe23c5447fc2bc22d61bf9d0db245
Daniel TI <danirmendo@gmail.com>
2026-02-04 18:57:07 -0300

    Turret Test

--- Turret.py
+++ Turret.py
@@ -1,39 +1,14 @@
-import wpilib
 import rev
-import phoenix6
-from camera import AprilTagCamera
-from wpimath.controller import PIDController
 
 class Turret:
-    def __init__(self, motor_port):
-        self.shooter1 = rev.SparkMax(1, rev.SparkLowLevel.MotorType.kBrushless)
-        self.shooter2 = rev.SparkMax(1, rev.SparkLowLevel.MotorType.kBrushless)
-        self.yaw = phoenix6.hardware.TalonFX(1, "rio")
-        self.pitch = rev.SparkMax(1, rev.SparkLowLevel.MotorType.kBrushless)
+    def __init__(self):
+        self.turret_motor = rev.SparkMax(51, rev.SparkLowLevel.MotorType.kBrushless)
+      
+    def turnLeft(self):
+        self.turret_motor.set(0.2)
 
-        self.shooter = wpilib.MotorControllerGroup(self.shooter1, self.shooter2)
-        self.shooter1.setInverted(True)
+    def turnRight(self):
+        self.turret_motor.set(-0.2)
 
-        self.camera = AprilTagCamera()
-        self.pid_angular = PIDController(0.1, 0, 0)
-
-
-    def shooterSpeed(self, speed) -> None:
-        self.shooter.set(speed)
-
-    def yawLeft(self) -> None:
-        self.yaw.set(1)
-
-    def yawRight(self):    
-        self.yaw.set(-1)
-
-    def pitchUp(self) -> None:
-        self.pitch.set(1)
-
-    def pitchDown(self) -> None:
-        self.pitch.set(-1)    
-    
-    def aimAttTarget(self, tag : int) -> None:
-        yaw = self.camera.getYaw(tag)
-        turn = self.pid_angular.calculate(yaw, 0) if yaw != -1 else 0
-        self.yaw.set(turn)
+    def stopTurret(self):
+        self.turret_motor.set(0)       
--- robot.py
+++ robot.py
@@ -1,2 +1,19 @@
 import wpilib
+import Turret
+
+class MyRobot (wpilib.TimedRobot):
+
+    def robotInit(self):
+        self.torreta = Turret.Turret()
+        self.joystick = wpilib.Joystick(0)
+
+    def teleopPeriodic(self):
+
+        if self.joystick.getRawButton(1):
+            self.torreta.turnLeft()
+        elif self.joystick.getRawButton(2):
+            self.torreta.turnRight()    
+        else:
+            self.torreta.stopTurret()    
+
 
