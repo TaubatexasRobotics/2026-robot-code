3b28c3df1a236478eb7a4ba415dc14df1d454c57
Guilherme Samuel <guisamuel800@gmail.com>
2026-02-07 10:33:59 -0300

    feat: add system to tuning shooter feedforward

---
+++ shooter.py
@@ -0,0 +1,44 @@
+from rev import SparkMax, SparkLowLevel, ResetMode, PersistMode, SparkMaxConfig, SparkBaseConfig
+
+class Shooter:
+    def __init__(self, kS: int, kV: int, kA: int, setpoint: int) -> None:
+        self.motor = SparkMax(11, SparkLowLevel.MotorType.kBrushless)
+
+        config = SparkMaxConfig()
+
+        config.smartCurrentLimit(40)
+        config.setIdleMode(SparkBaseConfig.IdleMode.kCoast)
+
+        config.closedLoop.pid(0, 0, 0)
+        config.closedLoop.feedForward.kS(kS)
+        config.closedLoop.feedForward.kV(kV)
+        config.closedLoop.feedForward.kA(kA)
+        
+        self.motor.configure(
+            config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters
+        )
+
+        #self.controller = self.motor.getClosedLoopController()
+        #config.closedLoop.maxMotion.cruiseVelocity()
+        #config.closedLoop.maxAcceleration.cruiseVelocity()
+        #config.closedLoop.allowedProfileError.cruiseVelocity()
+
+        #self.motor.setSetpoint(setpoint, SparkLowLevel.ControlType.kMAXMotionVelocityControl)
+
+    def setFeedforwardConstraints(self, kS: int, kV: int, kA: int) -> None:
+        config = SparkMaxConfig()
+
+        config.smartCurrentLimit(40)
+        config.setIdleMode(SparkBaseConfig.IdleMode.kCoast)
+
+        config.closedLoop.pid(0, 0, 0)
+        config.closedLoop.feedForward.kS(kS)
+        config.closedLoop.feedForward.kV(kV)
+        config.closedLoop.feedForward.kA(kA)
+
+        self.motor.configure(
+            config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters
+        )
+
+    def activateShooter(self) -> None:
+        self.motor.set(0.7)
