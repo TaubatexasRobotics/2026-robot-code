09dad3660d62f998c70b373e508efef9322a2d27
Batista <danirmendo@gmail.com>
2026-02-21 11:26:06 -0300

    adcionado codigo pixy2

--- led-arduino/ws2812b_pixy2.ino
+++ led-arduino/ws2812b_pixy2.ino
@@ -1,8 +1,10 @@
 #include <FastLED.h>
+#include <Pixy2.h>
 #define LED_PIN 7
 #define NUM_LEDS 30
 
 CRGB leds[NUM_LEDS];
+Pixy2 pixy;
 
 void changeColor(int red, int green, int blue) {
   for (int i = 0; i < NUM_LEDS; i++)
@@ -14,9 +16,13 @@ void setup() {
   Serial.begin(9600);
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   changeColor(255, 0, 0);
+  pixy.init();
 }
 
 void loop() {
+  {int i;
+  pixy.ccc.getBlocks();
+
   if (Serial.available() > 0) {
     byte received = Serial.read();
     if (received == 'r') {
@@ -25,5 +31,19 @@ void loop() {
       changeColor(0, 255, 0);
     else if (received == 'b')
       changeColor(0, 0, 255);
+    }
   }
+}
+  if (pixy.ccc.numBlocks)
+  {
+    Serial.print("Detected ");
+    Serial.println(pixy.ccc.numBlocks);
+    for (i=0; i<pixy.ccc.numBlocks; i++)
+    {
+      Serial.print("  block ");
+      Serial.print(i);
+      Serial.print(": ");
+      pixy.ccc.blocks[i].print();
+    }
+  }  
 }
