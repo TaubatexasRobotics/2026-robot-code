088b548ed6036386bb3d4378ff68bdd82e64051e
Danone <luccavanone1@gmail.com>
2026-03-01 19:13:12 -0300

    feat: add hood feedforward calculation as arm

--- robot.py
+++ robot.py
@@ -1,25 +1,22 @@
 import wpilib
-from crest import Crest
+from shooter import Shooter
 
 class MyRobot(wpilib.TimedRobot):
 
     def robotInit(self):
-        self.crest = Crest()
+        self.hood = Shooter()
         self.joystick = wpilib.Joystick(0)
         self.dashboard = wpilib.SmartDashboard
         wpilib.SmartDashboard.putNumber("setpoint", 0)
 
 
     def teleopPeriodic(self):
-        self.crest.update_dashboard()
+        self.hood.update_dashboard()
 
-        self.crest.move_to_setpoint()
+        if self.joystick.getRawButton(1):
+            self.hood.up()
+        elif self.joystick.getRawButton(2):
+            self.hood.down()
+        else:
+            self.hood.stop()    
 
-
-        # if self.joystick.getRawButton(1):
-        #   self.crest.up()
-        # elif self.joystick.getRawButton(2):
-        #   self.crest.down()
-        # else:
-        #     self.crest.stop()
-        
--- shooter.py
+++ shooter.py
@@ -1,20 +1,23 @@
 import rev
 import wpimath.controller
 from wpilib import SmartDashboard
+import math  # ADICIONADO para converter graus -> radianos
 
 gear_ratio = 11.52
 
 def clamp(value, min_value, max_value):
     return max(min(value, max_value), min_value)
 
-class Crest:
+class Shooter:
     def __init__(self):
         self.motor = rev.SparkMax(53, rev.SparkMax.MotorType.kBrushless)
         self.encoder = self.motor.getEncoder()
 
-        self.pid = wpimath.controller.PIDController(0.006, 0.0, 0.0)
+        self.pid = wpimath.controller.PIDController(0.01, 0.0, 0.0)
         self.pid.setTolerance(0.1)
         self.encoder.setPosition(0)
+
+        self.feedforward = wpimath.controller.ArmFeedforward(0.0, 0.5, 0.0, 0.0)  # ADICIONADO kG inicial = 0.05
 
     
     def getPosition(self) -> float:
@@ -24,18 +27,29 @@ class Crest:
         SmartDashboard.putData("PID", self.pid)
         SmartDashboard.putNumber("crest encoder", self.encoder.getPosition())
         SmartDashboard.putNumber("crest position", float(self.getPosition()))
-        # self.pid.setSetpoint(self.setpoint)
         print(self.pid.getSetpoint())
 
     def move_to_setpoint(self):
         current_position = self.getPosition()
         motor_value = self.pid.calculate(current_position)
+
+        angle_radians = math.radians(self.pid.getSetpoint())  # ADICIONADO conversão para feedforward
+        ff = self.feedforward.calculate(angle_radians, 0)  # ADICIONADO cálculo do kG
+
+        motor_value = motor_value + ff  # ADICIONADO soma do feedforward
+
         motor_value = clamp(motor_value, -0.4, 0.4)
         self.motor.set(motor_value)
 
     def move_to(self, setpoint):
         current_position = self.getPosition()
         motor_value = self.pid.calculate(current_position, setpoint)
+
+        angle_radians = math.radians(setpoint)  # ADICIONADO conversão para feedforward
+        ff = self.feedforward.calculate(angle_radians, 0)  # ADICIONADO cálculo do kG
+
+        motor_value = motor_value + ff  # ADICIONADO soma do feedforward
+
         motor_value = clamp(motor_value, -0.4, 0.4)
         self.motor.set(motor_value)
 
@@ -46,10 +60,10 @@ class Crest:
         self.move_to(0)
         
     def subir(self):    
-      self.motor.set(0.2)
+      self.motor.set(1)
     
     def descer(self):
-        self.motor.set(-0.05)
+        self.motor.set(-0.7)
 
     def stop(self):
         self.motor.stopMotor()
