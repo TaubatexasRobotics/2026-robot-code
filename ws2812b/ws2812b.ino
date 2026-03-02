f8cfd3863756b03df22c9f287399a7b9a6ebb40b
Guilherme Samuel <guisamuel800@gmail.com>
2026-03-02 15:11:18 -0300

    refactor: revert arduino ws2812b led code

--- ws2812b/ws2812b.ino
+++ ws2812b/ws2812b.ino
@@ -1,11 +1,9 @@
 #include <FastLED.h>
-#include <Pixy2.h>
 
-#define LED_PIN 7
+#define LED_PIN 3
 #define NUM_LEDS 30
 
 CRGB leds[NUM_LEDS];
-Pixy2 pixy;
 
 void changeColor(int red, int green, int blue) {
   for (int i = 0; i < NUM_LEDS; i++)
@@ -13,32 +11,15 @@ void changeColor(int red, int green, int blue) {
   FastLED.show();
 }
 
-void readPixyData() {
-  int i;
-  pixy.ccc.getBlocks();
-
-  if (pixy.ccc.numBlocks) {
-    Serial.print("Detected ");
-    Serial.println(pixy.ccc.numBlocks);
-    for (i = 0; i < pixy.ccc.numBlocks; i++) {
-      Serial.print("  block ");
-      Serial.print(i);
-      Serial.print(": ");
-      pixy.ccc.blocks[i].print();
-    }
-  }
-}
-
 void updateSelectedColor() {
   if (Serial.available() > 0) {
     byte received = Serial.read();
-    if (received == 'r') {
+    if (received == 'r')
       changeColor(255, 0, 0);
     else if (received == 'g')
       changeColor(0, 255, 0);
     else if (received == 'b')
       changeColor(0, 0, 255);
-    }
   }
 }
 
@@ -46,7 +27,6 @@ void setup() {
   Serial.begin(9600);
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   changeColor(255, 0, 0);
-  pixy.init();
 }
 
 void loop() {
