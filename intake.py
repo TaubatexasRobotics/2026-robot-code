82bf90e82b57bb61b9c61c4cc81113b0e4e5dec2
Flávio de Lima <flaviolimpio@gmail.com>
2026-03-03 18:19:54 -0300

    feat: add dashboard and move teleop control to mechanism files

--- intake.py
+++ intake.py
@@ -1,14 +1,19 @@
 import rev
 import phoenix5
+import wpilib
+from wpilib import SmartDashboard
 
 ARM_MOTOR_ID = 53
 ROLL_MOTOR_ID = 12
 
+joystick = wpilib.Joystick(0)
+
 class Intake:
     def __init__(self):
+        self.is_enabled = False
         self.arm_motor = rev.SparkMax(ARM_MOTOR_ID, rev.SparkLowLevel.MotorType.kBrushless)
         self.roll_motor = phoenix5.WPI_VictorSPX(ROLL_MOTOR_ID)
-
+        
     def turnDown(self):
         self.arm_motor.set(-0.7)
 
@@ -26,3 +31,26 @@ class Intake:
 
     def drop(self):
         self.roll_motor.set(1)
+        
+    def teleopPeriodic(self):
+        try:
+            if joystick.getRawButtonPressed(1):
+                self.is_enabled = not self.is_enabled  
+
+            if self.is_enabled:
+                self.receive()
+            else:
+                if joystick.getRawButton(4):
+                    self.drop()
+                else:
+                    self.stopRoll()
+
+            if joystick.getRawButton(2):
+                self.turnDown()
+            elif joystick.getRawButton(3):
+                self.turnUp()
+            else:
+                self.stopArm()
+                
+        except BaseException as e:
+            wpilib.DataLogManager.log(repr(e))
--- robot.py
+++ robot.py
@@ -2,58 +2,40 @@ import wpilib
 from intake import Intake
 from turret import Turret
 from drivetrain import Drivetrain
+from commands2 import TimedCommandRobot
 
 def log_exception(e):
     wpilib.DataLogManager.log(repr(e))
     
-class MyRobot(wpilib.TimedRobot):
+class MyRobot(TimedCommandRobot):
 
     def robotInit(self):
         self.intake = Intake()
         self.drivetrain = Drivetrain()
         self.turret = Turret()
         
+        self.mechanisms = [self.intake, self.drivetrain, self.turret]
+        
         self.joystick = wpilib.Joystick(0)
-        self.is_intake_enabled = False
+        self.intake.is_enabled = False
+        
+        wpilib.SmartDashboard.putNumber("test", 123)
+        
+    def robotPeriodic(self):
+        for mechanism in self.mechanisms:
+            try:
+                if hasattr(mechanism, "update_dashboard"):
+                    mechanism.update_dashboard()
+            except BaseException as e:
+                log_exception(e)        
     
     def teleopInit(self):
         pass
 
     def teleopPeriodic(self):
         try:
-            if self.joystick.getRawButtonPressed(1):
-                self.is_intake_enabled = not self.is_intake_enabled  
+            self.intake.teleopPeriodic()
 
-            if self.is_intake_enabled:
-                self.intake.receive()
-                print("sucking balls")
-            else:
-                if self.joystick.getRawButton(4):
-                    self.intake.drop()
-                    print("drop balls")
-                else:
-                    self.intake.stopRoll()
-                    print("stop roll")
-
-            if self.joystick.getRawButton(2):
-                print("arm down")
-                self.intake.turnDown()
-            elif self.joystick.getRawButton(3):
-                self.intake.turnUp()
-                print("arm up")
-            else:
-                self.intake.stopArm()
-                print("arm stopped")
-
-            # if self.joystick.getRawButton(2):
-            #     self.turret.shooterSpeed(.8)
-            # else:
-            #     self.turret.shooterSpeed(0)
-                
-            # if -0.1 > self.joystick.getRawAxis(4) < 0.1:
-            #     self.turret.yaw(0)
-            # else:
-            #     self.turret.yaw(self.joystick.getRawAxis(4))
         except BaseException as e:
             log_exception(e)
 
