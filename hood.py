2039ae7d16b415523b592760419273ec4bcc6bc6
Flávio de Lima <flaviolimpio@gmail.com>
2026-03-04 20:57:18 -0300

    feat: rename crest to hood and set controlls to drivers preferences

--- hood.py
+++ hood.py
@@ -1,17 +1,17 @@
 from utils import clamp
 import rev
 from wpimath.controller import PIDController, ArmFeedforward
-from wpilib import SmartDashboard, DataLogManager
+from wpilib import SmartDashboard
 
 gear_ratio = 11.52
-CREST_MOTOR_ID = 53
+HOOD_MOTOR_ID = 53
 
 MIN_VOLTAGE = -7
 MAX_VOLTAGE = 7
 
-class Crest:
+class Hood:
     def __init__(self):
-        self.motor = rev.SparkMax(CREST_MOTOR_ID, rev.SparkMax.MotorType.kBrushless)
+        self.motor = rev.SparkMax(HOOD_MOTOR_ID, rev.SparkMax.MotorType.kBrushless)
         self.encoder = self.motor.getEncoder()
 
         self.pid = PIDController(.006, .0, .0)
@@ -19,12 +19,12 @@ class Crest:
         self.pid.setTolerance(0.1)
         self.encoder.setPosition(0)
         
-        SmartDashboard.putNumber("Crest/FF/Ka", self.ff.getKa())
-        SmartDashboard.putNumber("Crest/FF/Kg", self.ff.getKg())
-        SmartDashboard.putNumber("Crest/FF/Kv", self.ff.getKv())
-        SmartDashboard.putNumber("Crest/FF/Ks", self.ff.getKs())
+        SmartDashboard.putNumber("Hood/FF/Ka", self.ff.getKa())
+        SmartDashboard.putNumber("Hood/FF/Kg", self.ff.getKg())
+        SmartDashboard.putNumber("Hood/FF/Kv", self.ff.getKv())
+        SmartDashboard.putNumber("Hood/FF/Ks", self.ff.getKs())
         
-        SmartDashboard.putNumber("Crest/set voltage", 7)
+        SmartDashboard.putNumber("Hood/set voltage", 7)
         
     def set_position(self, position):
         self.encoder.setPosition(position)
@@ -39,10 +39,10 @@ class Crest:
         return self.encoder.getVelocity() * gear_ratio * 360 / 60
 
     def update_dashboard(self):
-        SmartDashboard.putData("Crest/PID", self.pid)
-        SmartDashboard.putNumber("Crest/read voltage", self.get_voltage())
-        SmartDashboard.putNumber("Crest/encoder", self.motor.getEncoder().getPosition())
-        SmartDashboard.putNumber("Crest/angle", self.get_angle())
+        SmartDashboard.putData("Hood/PID", self.pid)
+        SmartDashboard.putNumber("Hood/read voltage", self.get_voltage())
+        SmartDashboard.putNumber("Hood/encoder", self.motor.getEncoder().getPosition())
+        SmartDashboard.putNumber("Hood/angle", self.get_angle())
         
     def set_voltage(self, voltage):
         self.motor.setVoltage(voltage)
@@ -58,12 +58,12 @@ class Crest:
         self.motor.setVoltage(motor_voltage)
 
     def move_to(self, setpoint):
-        '''Moves the crest to the given angle in radians.'''
+        '''Moves the Hood to the given angle in radians.'''
         self.pid.setSetpoint(setpoint)
         self.move_to_setpoint()
         
     def teleopPeriodic(self):
-        voltage = SmartDashboard.getNumber("Crest/set voltage", 0)
+        voltage = SmartDashboard.getNumber("Hood/set voltage", 0)
         if voltage != 0:
             self.set_voltage(voltage)
         else:
