33531af7cda77816d6eca091ec348f709721585a
Guilherme Samuel <guisamuel800@gmail.com>
2026-03-05 04:50:09 -0300

    feat: add blink green and rainbow led combination colors options

--- led.py
+++ led.py
@@ -6,6 +6,7 @@ class LEDController(Subsystem):
     def __init__(self) -> None:
         self.arduino = SerialPort(constants.kBaudRate, constants.kLEDUSBPort)
         self.extraLED = Solenoid(PneumaticsModuleType.CTREPCM, 4)
+        self.status = "off"
 
     def red(self) -> Command:
         return self.run(lambda: self.changeColor("r"))
@@ -16,9 +17,18 @@ class LEDController(Subsystem):
     def blue(self) -> Command:
         return self.run(lambda: self.changeColor("b"))
 
+    def blinkGreen(self) -> Command:
+        return self.run(lambda: self.changeColor("w"))
+
+    def rainbow(self) -> Command:
+        return self.run(lambda: self.changeColor("a"))
+
     def setExtraLED(self, status: bool) -> None:
         return self.extraLED.set(status)
 
     def changeColor(self, char: str) -> None:
+        if self.status == char:
+            return
+        self.status = char
         byte_obj = char.encode("ascii")
         self.arduino.write(byte_obj)
--- ws2812b/ws2812b.ino
+++ ws2812b/ws2812b.ino
@@ -4,11 +4,28 @@
 #define NUM_LEDS 60
 
 CRGB leds[NUM_LEDS];
+uint8_t waveOffset = 0;
 
 void changeColor(int red, int green, int blue) {
   for (int i = 0; i < NUM_LEDS; i++)
     leds[i] = CRGB(red, green, blue);
   FastLED.show();
+}
+
+void rainbow() {
+  for(int i = 0; i < NUM_LEDS; i++) {
+    uint8_t wave = sin8(i * 12 + waveOffset);
+    leds[i] = CHSV(i * 5 + waveOffset, 255, wave);
+  }
+  FastLED.show();
+  waveOffset += 4;
+  delay(20);
+}
+
+void blinkGreen() {
+  changeColor(0, 255, 0);
+  delay(500);
+  changeColor(0, 0, 0);
 }
 
 void updateSelectedColor() {
@@ -20,6 +37,10 @@ void updateSelectedColor() {
       changeColor(0, 255, 0);
     else if (received == 'b')
       changeColor(0, 0, 255);
+    else if (received == 'w')
+      blinkGreen();
+    else if (received == 'a')
+      rainbow();
   }
 }
 
